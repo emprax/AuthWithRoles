63af801c8a1a6083df37855d4b5c796ffe339b76
simutaroman <simutaroman@gmail.com>
2017-09-06 21:15:34 +0300

    basic auth with roles app

---
+++ AuthWithRoles/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthWithRoles.Controllers
+{
+    [Authorize(Policy = "RequireAdminRole")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewData["PageID"] = "Admin";
+            ViewData["Message"] = "admin page restricted to the super admin user ";
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            ViewData["Message"] = "just another page restricted to the super admin user ";
+            return View();
+        }
+    }
+}
---
+++ AuthWithRoles/Data/DbInitializer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AuthWithRoles.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AuthWithRoles.Data
+{
+    public static class DbInitializer
+    {
+        public static async Task InitializeAsync(ApplicationDbContext context, IServiceProvider serviceProvider)
+        {
+            context.Database.EnsureCreated();
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Admin", "Manager", "Member" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            
+            var _user = await UserManager.FindByEmailAsync(config.GetSection("AppSettings")["UserEmail"]);
+
+            if (_user == null)
+            {
+                var poweruser = new ApplicationUser
+                {
+                    UserName = config.GetSection("AppSettings")["UserEmail"],
+                    Email = config.GetSection("AppSettings")["UserEmail"]
+                };
+
+                string UserPassword = config.GetSection("AppSettings")["UserPassword"];
+
+                var createPowerUser = await UserManager.CreateAsync(poweruser, UserPassword);
+                if (createPowerUser.Succeeded)
+                {
+                    await UserManager.AddToRoleAsync(poweruser, "Admin");
+                }
+            }
+        }
+    }
+}
